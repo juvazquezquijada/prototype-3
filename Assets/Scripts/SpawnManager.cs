741a0830e811bff904318d18d7dc8dfbcc6a2b04
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-02-15 11:03:55 -0800

    stopped move left when game over

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle ()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
